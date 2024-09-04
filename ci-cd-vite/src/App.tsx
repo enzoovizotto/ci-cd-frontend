8b3da612d25f8205f66468286c9706b40ab06fe2
Enzo Vizotto <enzo.vizotto@tech4h.com.br>
2024-09-04 10:33:52 -0300

    edit text

--- ci-cd-vite/src/App.tsx
+++ ci-cd-vite/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Enzo alterou o campo aqui
         </p>
       </div>
       <p className="read-the-docs">
